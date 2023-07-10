c38918f511e5ccc3e1b73feb45ae20a629ef4d36
Zaiyab <zaiyabdati@gmail.com>
2023-07-10 17:24:32 +0500

    fixed error

---
+++ src/Components/NewsItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        This is item
+      
+      </div>
+    )
+  }
+}
+
+export default NewsItem

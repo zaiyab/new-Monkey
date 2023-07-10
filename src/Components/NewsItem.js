ac83fa0e969a935a531b63582d9410f5f54997c2
zaiyab <zaiyabdati@gmail.com>
2023-07-11 01:34:17 +0500

    fetch news

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {img,title,desc} = this.props
+    let {urlimg,title,desc,newsurl,originallength,date} = this.props
     return (
-      <div>
+      <div className='my-3'>
         <div className="card" style={{width: '18rem'}}>
-            <img src={img} className="card-img-top" alt="..."/>
+            <img src={urlimg} className="card-img-top" alt="..."/>
             <div className="card-body">
-              <h5 className="card-title">{title}</h5>
-              <p className="card-text">{desc}</p>
-              <a href="/" className="btn btn-primary">Go somewhere</a>
+              <h5 className="card-title">{title}{originallength.length>45?'....':''}</h5>
+              <p className="card-text">{desc}....</p>
+              <p><strong>Publish Date: </strong>{date}</p>
+              <a href={newsurl} target='_blank' className="btn btn-sm btn-primary">Read More</a>
          </div>
 </div>
       

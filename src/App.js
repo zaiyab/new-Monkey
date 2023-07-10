dbd1abc3eb9cfdc168ffe319a74dc531d5331238
Zaiyab <zaiyabdati@gmail.com>
2023-07-10 17:17:09 +0500

    fixed error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import Navbar from './Components/Navbar'
+import News from './Components/news'
+
+
 
 export class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <News />
       </div>
     )
   }

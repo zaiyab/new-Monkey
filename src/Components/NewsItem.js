1af48ab0b50e44906914972f19e147dbd4b49034
zaiyab <zaiyabdati@gmail.com>
2023-07-10 20:19:04 +0500

    componentes update added cards

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {img,title,desc} = this.props
     return (
       <div>
-        This is item
+        <div className="card" style={{width: '18rem'}}>
+            <img src={img} className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{desc}</p>
+              <a href="/" className="btn btn-primary">Go somewhere</a>
+         </div>
+</div>
       
       </div>
     )
--- src/Components/news.js
+++ src/Components/news.js
@@ -2,14 +2,33 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+  
   render() {
+ 
+     let  img="https://content.api.news/v3/images/bin/7b6356501373db5a91b909f77299361a"
+     let title='Image'
+     let desc= "News"
     return (
-      <div>
-        this is component
-        <NewsItem />
+      
+     <div>
+           <div className='container my-3 py-3'>
+                <h1>NewsMonkey - top headlines</h1>
+                     <div className='row my-3'>
+                            <div className='col-md-4'>
+                            <NewsItem title={title} img = {img} desc = {desc}/>
+                            </div>
+                            <div className='col-md-4'>
+                            <NewsItem title={title} img = {img} desc = {desc}/>
+                            </div>
+                            <div className='col-md-4'>
+                            <NewsItem title={title} img = {img} desc = {desc}/>
+                            </div>
+                   </div>
+             </div>
       </div>
     )
   }
 }
 
 export default News
+

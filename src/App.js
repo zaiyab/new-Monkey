21c459c05db1e8c9fdeca733003e94adf5eb2bba
zaiyab <zaiyabdati@gmail.com>
2023-07-13 00:02:26 +0500

    tut 37 end

--- src/App.js
+++ src/App.js
@@ -1,43 +1,124 @@
-import React, { Component } from 'react'
-import Navbar from './Components/Navbar'
-import News from './Components/news'
+import React, { Component } from "react";
+import Navbar from "./Components/Navbar";
+import News from "./Components/news";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import LoadingBar from "react-top-loading-bar";
 
 export class App extends Component {
-pagesize = 15
+  pagesize = 15;
+  apiKey = "fc4524215a4d46da8f00d7a2bfb3d968";
+  state = { progress: 0 };
+
+  setProgress = (prog) => {
+    this.setState({ progress: prog });
+  };
+
   render() {
     return (
       <div>
         <Router>
           <Navbar />
+          <LoadingBar color="#f11946" progress={this.state.progress} />
           <Routes>
+            <Route
+              path="/"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  apiKey={this.apiKey}
+                  pageSize={this.pagesize}
+                  key="gemeral"
+                  country="us"
+                  cat="general"
+                />
+              }
+            />
+            <Route
+              path="/business"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="busness"
+                  apiKey={this.apiKey}
+                  pageSize={this.pagesize}
+                  country="us"
+                  cat="business"
+                />
+              }
+            />
+            <Route
+              path="/entertainment"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="entertainment"
+                  apiKey={this.apiKey}
+                  pageSize={this.pagesize}
+                  country="us"
+                  cat="entertainment"
+                />
+              }
+            />
 
-            <Route path="/" element={<News pageSize={this.pagesize} country='us' cat="general" />} />
-            <Route path="/business" element={<News key="busness" pageSize={this.pagesize} country='us' cat="business" />} />
-            <Route path="/entertainment" element={<News key="entertainment" pageSize={this.pagesize} country='us' cat="entertainment" />} />
+            <Route
+              path="/health"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="health"
+                  apiKey={this.apiKey}
+                  pageSize={this.pagesize}
+                  country="us"
+                  cat="health"
+                />
+              }
+            />
 
-            <Route path="/health" element={<News key="health" pageSize={this.pagesize} country='us' cat="health" />} />
+            <Route
+              path="/science"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="science"
+                  apiKey={this.apiKey}
+                  pageSize={this.pagesize}
+                  country="us"
+                  cat="science"
+                />
+              }
+            />
+            <Route
+              path="/sports"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="sports"
+                  apiKey={this.apiKey}
+                  pageSize={this.pagesize}
+                  country="us"
+                  cat="sports"
+                />
+              }
+            />
 
-            <Route path="/science" element={<News key="science" pageSize={this.pagesize} country='us' cat="science" />} />
-            <Route path="/sports" element={<News key="sports" pageSize={this.pagesize} country='us' cat="sports" />} />
-
-            <Route path="/technology" element={<News key="technology" pageSize={this.pagesize} country='us' cat="technology" />} />
-
-
-
-
-
-
-
+            <Route
+              path="/technology"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="technology"
+                  apiKey={this.apiKey}
+                  pageSize={this.pagesize}
+                  country="us"
+                  cat="technology"
+                />
+              }
+            />
           </Routes>
         </Router>
-
-
-
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,23 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { urlimg, title, desc, newsurl, originallength, date, author } = this.props
+    let { urlimg, title, desc, newsurl, originallength, source, date, author } =
+      this.props;
 
     return (
-      <div className='my-3'>
-        <div className="card" style={{ width: '20rem' }}>
+      <div className="my-3">
+        <div className="card" style={{ width: "20rem" }}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: "0",
+            }}
+          >
+            <span className="badge rounded-pill bg-danger">{source}</span>
+          </div>
           <img src={urlimg} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}{originallength.length > 45 ? '....' : ''}</h5>
+            <h5 className="card-title">
+              {title}
+              {originallength.length > 45 ? "...." : ""}
+            </h5>
             <p className="card-text">{desc}....</p>
-            <p>By <strong>{author}</strong> at {new Date(date).toDateString()}</p>
-            <a href={newsurl} target='_blank' className="btn btn-sm btn-dark">Read More</a>
+            <p>
+              By <strong>{author}</strong> at {new Date(date).toDateString()}
+            </p>
+            <a href={newsurl} target="_blank" className="btn btn-sm btn-dark">
+              Read More
+            </a>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
